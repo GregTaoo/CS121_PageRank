fd94f7c7fb737d59a203c6bac3cc64501fab6c38
GregTao <gregtaoo@outlook.com>
2025-12-23 17:14:55 +0800

    pr normalized

--- pagerank_omp.c
+++ pagerank_omp.c
@@ -12,15 +12,18 @@ double *pagerank_omp(const int num_threads, const graph *g, const graph *convers
   const int n = g->v;
 
   double *pr_new = aligned_alloc(64, sizeof(double) * n);
-  double *out_w  = malloc(sizeof(double) * n);
-  memset(out_w, 0, sizeof(double) * n);
+  int *out_w = malloc(sizeof(int) * n);
+  memset(out_w, 0, sizeof(int) * n);
+  double *pr_normalized = aligned_alloc(64, sizeof(double) * n);
 
 #pragma omp parallel for schedule(static)
   for (int u = 0; u < n; u++) {
     pr[u] = 1.0 / n;
+    int w = 0;
     for (int i = g->offset[u]; i < g->offset[u + 1]; i++) {
-      out_w[u] += g->m[i].w;
+      w += g->m[i].w;
     }
+    out_w[u] = w;
   }
   double dangling_sum = 0.0;
 
@@ -39,13 +42,23 @@ double *pagerank_omp(const int num_threads, const graph *g, const graph *convers
 
 #pragma omp parallel
     {
-#pragma omp for schedule(dynamic, 16) nowait
+#pragma omp for schedule(static)
+      for (int i = 0; i < n; i++) {
+        if (out_w[i] > 0) {
+          pr_normalized[i] = pr[i] * damping / (double) out_w[i];
+        } else {
+          pr_normalized[i] = 0.0;
+        }
+      }
+
+#pragma omp for schedule(dynamic, 64)
       for (int u = 0; u < n; u++) {
         double sum = (1.0 - damping) / n + dangling_contrib;
-        for (int i = converse->offset[u]; i < converse->offset[u + 1]; i++) {
+        const int start = converse->offset[u], end = converse->offset[u + 1];
+        for (int i = start; i < end; i++) {
           const int v = converse->m[i].v;
-          const double w = converse->m[i].w;
-          sum += damping * pr[v] * (w / out_w[v]);
+          const int w = converse->m[i].w;
+          sum += w * pr_normalized[v];
         }
         pr_new[u] = sum;
       }
@@ -65,12 +78,11 @@ double *pagerank_omp(const int num_threads, const graph *g, const graph *convers
     dangling_sum = new_dangling_sum;
 
     if (diff < eps) {
-      // printf("Converged at iter %d\n", iter);
       break;
     }
   }
 
-  // free(start_v);
+  free(pr_normalized);
   free(pr_new);
   free(out_w);
   return pr;
